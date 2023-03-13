7a91ed1eed9b062a0298f78e2746aff812f4e3bd
xiayue_fe <xiayue_fe@hotmail.com>
2023-03-13 12:02:25 +0800

    Add files via upload

---
+++ kaisya.js
@@ -0,0 +1,1074 @@
+const str = `○辱堂
+10mile
+13cc
+13cm
+18ロケットアイスクリーム
+48Te
+4H（フォーエイチ）
+5bit
+7th Door（セブンスドア）
+A.S.S
+Aaru
+ABALONE KISS
+ABALONE KISS（アバロンキス）
+abelia
+Acme・X
+ACONITE
+AHAAN
+aias
+AIRRIP
+ALcot
+ALICESOFT
+ALL-TiME
+Alo-pierce
+AMBER
+AMOLPHAS
+AMUSE CRAFT EROTICA
+An*tique
+Angel Black（エンジェル ブラック）
+Angel SOFT
+AngelSmile
+ANIM
+ANIM Mother＆Wife
+ANIM Official Website
+ANIM.teamMM
+Anime Lilith
+Anime-seal
+Another Room
+Ape＆Frog（エイプ・アンド・フロッグ）
+appleC.C.（あっぷるしーつー）
+Apricot Cherry
+AQNOS（アクノス）
+AQUAHOUSE
+Argonauts
+ArkShell
+ARMADILLO
+Artel
+ARTEMIS.
+Atelier G/H
+AVANTGARDE（アヴァンギャルド）
+B-LUSTER
+BaseSon
+BaseSon Light
+BEe Light
+BeF
+BELL-DA
+BELLOTA（ベジョータ）
+BISHOP
+BISHOP Royal
+Bitter half
+Black Cyc
+BLACK FIRE
+Black Lilith
+Black LUNA
+BLACK PACKAGE
+BLACK PACKAGE TRY
+BLACK PACKAGE XXX
+Black Rabbit
+BLACKGLOBE
+Blackluck
+BLACKRAINBOW
+BLACKRAINBOW with ニコチンソフト
+Blame
+Blue Devil（ブルー デビル）
+BlueTopaz
+bolero
+BOM
+BOM+xyz
+Bonbee！
+Bonk！GAME STUDIO
+bootUP!
+Bullet
+C:drive
+C：drive.
+C：drive./F
+C.C CLOCKUP
+Cadath
+CAGE（ケージ）
+Calcite
+Calcle
+CALIGULA
+CandySoft
+CARMINE
+CARMINE（deeperチーム）
+CARMINE（たっちー）
+Carol Works
+Casket
+CATTLEYA
+catwalk
+catwalkNERO
+catwalkNERO（キャットウォーク・ネロ）オフィシャルサイト
+Cellworks Corporation – 浮士德の旧作收集馆
+CHAOS-L
+CHAOS-R
+CHAOS-R EXTREME
+CHAOS-R Re:Master 
+CHAOS-R feat. Freak Strike 
+FREAK STRIKE
+（カオスアール カオスアール エクストリーム カオスアール リマスター カオスアール フィート フリークストライク フリークストライク）
+chiffon（シフォン）
+chococo
+cico（シコ）
+Ciel
+CINEMATOGRAPH
+Citrus
+CLIP☆CRAFT
+CLOCKUP
+CLOCKUP team.ANISE
+CLOCKUP team.DYO
+CLOVER
+CODEPINK
+COIN-SOFTWARE
+COLLECTION
+CONCEPT
+Concerto（コンチェルト）
+COSMOS
+CourregesA
+CreamPie
+Crepe
+Crepe（クレープ）
+Crime
+Cronus
+Crossnet-Pie
+CROSSOVER
+crownos（クロウノス）
+CRYSTALiA
+CUBE
+CUFFS
+CURIOUS（キュリウス）
+Cybele（キュベレ）
+Cyc
+CYC NO-NOS
+CYCLET
+cyon（サイオン）
+D-Angel
+D-XX
+D：drive.
+D’z
+Dark Side
+dark.ryona.x15（ダークリョナエックスジュウゴ）
+DarknessPot
+DARKROSE（ダークローズ）
+DarkShelf
+DD・T
+DD・T-Trois（トロア）
+DEEPBLUE
+deeper
+Delta
+DERUT！（デるっ）
+DESSERT Soft
+Devil-seal
+DEVIL’S WORKS
+DiGination
+Digital Cute
+DisAbel
+DISCOVERY
+DISCOVERY MARS
+DISTORTION
+DISTORTION INTERACTIVE
+DMM.com
+DOLCE
+DreamSoft
+Drei
+Dual Tail
+DUEE
+DWARFSOFT
+easter
+eelcom（エルコム）
+EGIS（アイギス）
+Eleonora
+Empress
+Empress×elf
+Empty
+ensemble
+ensemble SWEET
+EROGOS
+eRONDO
+Eroro
+EROTICA BLACK
+EROTICA PEACH
+ERROR
+ES-pot（エスポット）
+EUROS
+eushully エウシュリー
+EVE
+evee（エヴィ）
+evee（エヴィー）
+EvilHeal
+evoLL
+EXA
+EXA feat. 戯画
+Exception
+extern
+ExTry（エクストライ）
+eye★phon
+F＆C
+F＆C・FC01
+Factor
+feng
+FIANCEE
+Finish!
+FireDoor
+flap
+FlyingShine
+FlyingShine犬
+FlyingShine黒
+FlyingShine寿
+forte
+FREAK STRIKE
+Freakshow
+FREE RANCH（フリーランチ）
+Frill
+FROZEN
+FUCKソフト（ファックソフト）
+FULLTIME
+future
+g-clef
+G.J？
+Gage
+GAIA
+GALACTICA
+Galette
+Garden
+Gargoyle（ガーゴイル）
+Gash
+GATSUN SOFT
+Gear Stone Software（ギアストーンソフトウェア）
+GENDAI
+GLASSES
+Glastonbury
+GLOBE
+GOKU-RAKU
+Gracious
+Grand Cru
+Grand Cru bourgeois
+Grand Cru noir
+GRAY ZONE
+Gsoft（ジーソフト）
+GT-FOUR
+Guilty
+Guilty dash
+Guilty eX
+Guilty Nightmare Project
+Guilty+
+Guilty×material
+GuiltyN
+H・WORKS
+H＋（エイチプラス）
+Hanmmerheads
+HEAT-SOFT
+Heliodor
+Hending
+HERENCIA
+HERVEST
+hi-way
+hibiki works
+HighRunning
+Honey Dip
+HOOKSOFT
+hourglass
+HULOTTE
+Hybrid Balloon
+Hypersthene
+ILLUSION
+IMAGE-WORKS
+include
+Infinity ∞
+Innocennce
+Innocent Grey
+INTERHEART - top
+INTERHEART Cute
+INTERHEART glossy
+Island Belle
+JADE
+JELLYBEAN
+Jellyfish（ジェリーフィッシュ）
+JOKER
+JUICE
+KarinProject
+kelp
+KENZsoft
+KISS
+Kiss next
+KISS-MA
+KLEINシロップ
+Lamia
+LAPIS BLUe.
+Laplacian
+Lass Lunatic
+Lass Pixy
+LAZYCREW
+Le.Chocolat meets FlyingShine
+Le.Chocolat（ル・ショコラ）
+Le.Chocolat＆Raccoon
+Le.Chocolat×KLINE
+LiBi
+LIBIDO
+light
+LiLiM
+LiLiM DARKNESS
+LiLiM 製品情報
+LiLiM/CHOIR
+LiLiM生/H.W.Lab（ウエスト研究所）
+LiLiM生/LoveJuice
+LiLiM生/sunshine61
+LiLiM生/SuppleEntertainment
+Lilith
+Lilith [リリス]（リリス）
+Lilith Mist
+LimeLight
+Lipstick
+Liquid-Shine
+Liquid-Touchable
+Liquid（team Shadow）
+Liquid（リキッド）
+LO-LILITH
+Lost Heart
+Lost Script
+LoveJuice
+LoveJuiceピンク
+LOVERSOUL
+Lucha Libre
+LUCHA！
+Luna Prism
+Lust
+Lust Bit
+Lusterise
+Luxury Tiara
+Luxury（ラグジュアリー）
+M de PINK
+M na RED
+M ni AQUA
+M no VIOLET
+M-O
+MAGI
+MAGI black
+MAGI white
+Magical☆Girl
+Magical☆Girl（マジカルガール）
+MAIKA
+MAMAMAX
+maple
+MarryBell
+material
+material mel
+May-Be SOFT
+MBS TRUTH
+MBS TRUTH -Cherish Pink-
+MBS TRUTH -DeepBlue-
+MEGAMI
+MELLOW
+Melody
+Mercure（メルキュア）
+Meta-Doll
+metalogiq
+microGroove
+Miel
+MilkyKiss
+mini
+mini-mam
+MINK / M no VIOLET
+Mink EGO
+minori
+mirage
+mirai
+Misty
+mixwill soft
+Molamola.software
+MONAKO
+monoceros＋
+monoceros＋黒
+Monogram
+monomanie
+MOONSTONE
+MOONSTONE Cherry
+MOONSTONE Honey
+MORE
+MORE＆NIKO 青春アンソロジー関連
+MOSAIC
+MotherGoose
+Moviendo（モヴィエンド）
+Muscadet（ミュスカデ）
+My Daughter
+MyTreasure
+Mの時間
+Nail
+Nail Sharp
+Nameless
+NanaWind
+Navel
+NEGALION
+Nexton – 浮士德の旧作收集馆
+NIGRED（ニグレド）
+NIKO
+ninetail
+ninetail/dualtail（ナインテイル/デュアルテイル）
+NO BRAND
+NOA
+Noesis
+NOIR
+Nomad
+Norn
+North Box
+Nostalgic Chord
+Notus
+NTRイズム（ネトラレイズム）
+oct works
+OLE
+OLE-M
+ONE GAMES
+ONE-SHOT
+One-up
+onomatope*
+onomatope* raspberry
+OPTiM
+ORBIT-CACTUS
+Orthros
+OVER
+OVERDRIVE
+Oz Project
+P-factory
+PacoPacoSoft＋
+PADLOCK（パドロック）
+PANDORA
+Paranoia/Fact？
+PARTHENON
+Parthenon（パルテノン）
+PeasSoft
+PETALA
+PIL
+Pink Tissue
+pisckiss（ピスキッス）
+PlumZERO
+Pochette
+POISON
+POISON MOTION
+Poison＠Berry
+Ponytail Software
+Portion
+Potage
+Princess Sugar
+PriSorT
+Project-μ（ミュー）
+Psy-chs（サイクス）
+PSYCHO
+PULLTOP
+PULLTOP LATTE
+pure more
+Pure Platinum
+Pure-Liquid
+Pure☆Milk
+PURESIS
+Puzzlebox
+QRoss（キュロス）
+Qruppo
+R/A software
+Radi
+Rainbow Cyc
+RAPTOR
+RaSeN
+Rateblack
+RATI
+Rave’N
+Re-Pro！
+RE:creation
+Re：verse
+REAL
+REBECCA
+Red Label（レッドレーベル）
+Red Rebel（レッドレベル）
+RED-ZONE
+Remain
+Riddle Soft
+Ripe
+rootnuko
+rootnuko＋H
+rouge
+ruf
+RUNE
+RUNE Team TwinTail
+S.M.L
+SAGA PLANETS
+SAMOYED SMILE
+Saphir
+SATOR
+ScooP
+SCORE
+SCORE【しゅこあ！】
+SEACOXX
+seal-tutu
+seismic（サイズミック）
+Selen
+SelenADVANCE
+Shelf
+SherbetSoft（シャーベットソフト）
+sign
+Silhouette（シルエット）
+silica-gel（シリカゲル）
+Sister Position
+SKUNKWORKS
+SKUNKWORKS nano.
+Sky-High feat. 戯画
+SkyFish
+SkyFish poco
+SLAVEnir -スレイブニール- HP
+SMEE
+softhouse-seal
+softhouse-seal GRANDEE
+softhouse-seal（ソフトハウスシール）
+Soiree
+SPEED
+SPEED HYBRID
+spiel
+SPINDLE
+sprite
+SQUEEZ
+Starlit Sky（スターリットスカイ）
+Stellamint
+STELLAR
+StillNoise（スティルノイズ）
+STONE HEADS
+STRIKES
+StrikeS（ストライクス）
+STRIPE feat. 戯画
+STUDiO B-ROOM
+studio blanket
+Studio e・go!
+Studio Ebisu
+Studio Insane
+STUDIO TRIUMPH（スタジオトライアンフ）
+STUDIO TRIUMPHのページ
+StudioMebius
+studioオルカ
+Studioなべ奉行
+STUDIO邪恋
+STUDIO邪恋 Megaera
+STUDIO邪恋 typeM
+sukaradog-スカラドギ-（スカラドギ）
+SUKARADOG（スカラドギ）
+Surf
+Sweet
+Sweet HEART
+SWEET＆TEA
+Sweetpain TEAM M/K
+SYOKU
+SYRUP -Honey Sweet-
+SYRUP -many milk-
+SYSTEM ROSE
+TABOO
+Tactics
+TAIL SKID
+TakeOut
+TAM’S
+Tangerine
+teamクリムゾン
+TearDrop
+TEATIME
+TechArts3D
+TETRATECH
+That’s
+TinkerBell
+TinkerBell チーム・ザット
+TinkerBell某組
+TOMATO
+tone work’s
+TOUCHABLE
+TranceSoft
+Trans
+Tri-Star
+Triangle
+Triangle-delta
+Triangle-τ（タウ）
+tritail
+Trois（トロワ）
+TRUE BLUE関連
+TRUST
+TRYSET
+TRYSET Break
+TRYSET Light
+TRYSET MAD
+Two Strike
+U・Me SOFT
+U10ME（ユーテンミー）
+UMA!
+UNDEAD
+unicorn-a
+Vanadis
+VARIETA（バリエッタ）
+VEGA
+Venus_S
+Virgin Cream
+VISION
+WAFFLE
+Waffle | 公式サイト
+WAFFLE（ワッフル）
+WAM
+WendyBell
+WestVision
+Whirlpool
+WINTERS
+WitchFlame
+Wonder Fool
+work01 of CourregesA Official website
+X[iks]（イクス）
+Xiguratt Works
+XUSE【本醸造】（ザウスホンジョウゾウ）
+Yatagarasu
+ZERO
+ZION
+ZyX
+アーカムプロダクツ
+アーカムプロダクツ / チーム暗黒媒体
+アージュ
+アーテル
+アーベルソフトウェア
+アイチェリー
+アイル
+アイル オフィシャル ホームページ
+アイル【チーム・Fun2】
+アイル【チーム・MAXi】
+アイル【チーム・Riva】
+アイル【チーム・SOIF】
+アイル【チーム・TATU】
+アイル【チーム・アイシス】
+アイル【チーム・ドレイク】
+アイル【チーム・ラヴリス】
+アイル【チーム・る～にぃ】
+あかべぇそふとすりぃ
+あかべぇそふとつぅ
+あくおちメロン（アクオチメロン）
+アクティブ
+あざらしそふと
+あざらしそふと＋1
+アスガルド
+アストロノーツ・アリア
+アストロノーツ・コメット
+アストロノーツ・シリウス
+あせろらぷらす
+あっぷるみんと（アップルミント）
+アトリエD
+アトリエかぐや
+アトリエかぐや BARE＆BUNNY
+アトリエかぐや Berkshire Yorkshire
+アトリエかぐや CheshireCat
+アトリエかぐや DREIZEHN
+アトリエかぐや Honky-Tonk Pumpkin
+アトリエかぐや TEAM Gassa-Q
+アトリエかぐや TEAM HEARTBEAT
+アトリエさくら
+アトリエさくら DARKNESS
+アトリエさくら eXtra
+アトリエさくら Team.NTR
+アトリエさくら こんまいすたじお
+アトリエさくら 露出*遊戯
+あねせん
+アパタイト
+アパタイト・アパダッシュ 公式Webサイト
+アパダッシュ
+アボガドパワーズ
+アメノムラクモ
+あらもーど
+アルスター
+あんあん亭（アンアンテイ）
+あんく
+アンダームーン
+アンダーリップ
+アンモライト
+イーアンツ
+イエローピッグ
+イグニッション
+いちひめ
+いちゃらぶ堂
+いぬすく（イヌスク）
+イノヘッドスタジオ
+インカローズ
+インスタントカメレオン
+インターハート
+インレ
+ヴィーナス
+ウィズドロップ
+ウィルテイム
+ういろうそふと
+ういんどみるコスモス
+うさぎソフト
+うたまろソフト
+うらら
+エイチソフト
+えくれあ
+エスクード
+えにしそふと
+エムズトイボックス
+エルフ
+エレクトリップ
+エロイット
+エロゲーホンポ
+おうちじかん
+オーガスト
+オーサリングヘヴン
+オーバードーズ
+オーバーフロー
+おとこの娘倶楽部
+おとこの娘倶楽部Z
+ガールズソフトウェア
+カウパー
+かえるそふと
+カカオ
+かすたーど
+カニスキー
+キャラメルBOX いちご味
+キャラメルBOX ミルク味
+きゅんソフト
+クラウド
+クリムゾンSS
+くるりアクティブ
+クレージュ
+クレージュエース
+クロシェット
+クロックアップ企画
+くろにゃん
+ゲーム開発ブログ
+けだものSOFT
+ケルト
+ケロQ
+けんじゃたいむ（ケンジャタイム） サークルプロフィール
+ゴールドリップ
+こぱんだ
+コンフィチュールソフト
+コンプリーツ
+サイクスJ
+ザウス
+ザウス【吟醸】
+ザウス【発泡】
+さくらぷりん（サクラプリン）
+サボテン
+じぃすぽっと
+シーディーブロス
+シェイプシフター
+しすたーそふと
+シャオシャオカッパ
+シャルラクプラス
+じゅらく
+しらたま
+シリウス
+しるき～ずこねくと
+シルキーズ
+シルキーズSAKURA
+シルキーズプラス
+シルキーズプラス A5和牛
+シルキーズプラス WASABI
+シロップ
+しろにゃん
+ズー
+スカイロケット
+スタジオ カメ
+スタジオエロハウス
+スタジオセピア
+すたじおちゃれん（スタジオチャレン）
+スタジオポーク サークルプロフィール
+すたじおもみじ（スタジオモミジ）
+スタジオ偽恋
+すたじお実験室
+スタジオ奪
+スタジオ奪ルージュ
+スタジオ桃華
+スタッフィング
+ストライクスDL
+スナック・ファクトリー
+スノー
+スピンドル
+すふれそふと
+スペルマニアックス
+スペルメーション (SEACOXX)
+スミレ
+すもも
+スレイブニール サークルプロフィール
+スワン
+スワンアイ
+スワンアイ＋
+スワンマニア
+せ・き・ら・ら
+セレス
+セレネ
+そうざいそふと（ソウザイソフト）
+ソーラレイ
+ゾーン/スペースワークス
+ソクラテス
+ソシエッタ
+ソニア
+ソフトウェアハウスぱせり
+ソフトさ～くるクレージュ
+ソフトさーくるクレージュ
+ソフトハウスキャラ
+ソフトハウスラーク
+ダークスワン
+だーくワン！
+たいれるPiccolo
+たっちー
+たぬきそふと
+たまくろ
+たまソフト
+チーム-アップルパイ
+チッコ
+ちゃいむ
+ちゃんぷる～
+チロル
+つぼみソフト
+つみきそふと
+つるみく
+つるみく -Another-
+ディーオー
+ディーゼルマイン
+ディーゼルマイン　オフィシャルウェブサイト
+でぃすぺあ（ディスペア）
+でこポン！
+デジアニメ・コーポレイション
+デジタルGパワー
+でぼの巣製作所
+テリオス
+とこはな
+どっ恋ソフト
+トラヴュランス
+トリプルエックス
+トルピード
+ナタ・デ・ココ
+にくきゅう
+ニトロプラス
+ヌキコレ
+ネガティブギャング
+ネル
+の～すとらいく
+ノアールソフト
+は～とふるママン (ALICESOFT)
+バーサーカー
+バーディーソフト
+ハートブリガン
+パープルソフトウェア
+ハイクオソフト
+ハイパースペース
+バグシステム
+ぱこぱこそふと
+ぱじゃまエクスタシー
+ぱちぱちそふと
+ぱちぱちそふと黒
+ハチミツ
+はちみつそふと
+ハニーボーイ
+はにぃま～る
+ハニーマリー
+バニラ
+はむはむソフト
+パルテノン
+ぱるふぁんそふと
+ぱれっと
+ぱんだはうす
+パンチラキック
+び～にゃん
+ピアス
+ビアンカ
+ピーキー
+ピオニー
+ぴたふぇち！
+ビタミン
+ひまわり
+ひよこソフト
+ひよこソフト桃組
+ぴよぴよ組
+ピンキィソフト
+ピンキィソフト ミッドナイトムーンメディア
+ぴんくはてな
+ピンパイ
+ピンパイジューシィ
+ピンヒール from lune
+ピンヒールfromルネ
+ピンポイント
+ピンポイント / キングピン / ピンポイントクイック（ピンポイントキングピンピンポイントクイック） サークルプロフィール
+ピンポイント オフィシャルホームページへようこそ！｜PinPoint
+ピンポイント/キングピン
+ファーサイド
+ファントムソフト
+フェアリーダスト
+フェアリーテール
+ふぇちっしゅ
+ふぇんりる
+フォスター
+ふくろう
+ぷちあんく
+ぷちうさ
+ぷちフェレット
+ぷちミルフィーユ
+ブラックカラント
+プリックリーアッシュ
+プリマ
+ブルーゲイル
+ブルゲ ON DEMAND
+ブルゲDL
+ブルゲLIGHT
+ブルゲはいぶりっど
+フルボトル
+プレカノ
+プロダクト | Lusterise
+フロントウイング
+ヘクセンハウス
+ペペ秘宝館（ペペヒホウカン）
+ベルゼブブ
+ペンギンワークス
+ポアシャラ
+ボクカノProject
+ポコブラック
+ホットピンク
+ホビコレ
+ホワイト
+ま～まれぇど
+マーブルCandySoft
+マーマレード★スター
+マウントポジション
+ましゅまろそふと
+マッセル
+マッドレイド
+まどそふと
+まどぼいす（マドボイス）
+マリン
+マンマミーア！
+みこにそみ
+みさくらなんこつハースニール（ミサクラナンコツハースニール）
+ミスティムーン
+ミスリル ソフトウェア
+みなとそふと
+みるきーポコ
+ミルクキャンディー
+みるくぱい
+みるくぱい（ミルクパイ）
+みるくふぁくとりー
+ミルフィーユ
+ミンク
+むつ組
+メイド倶楽部
+メガロマンス
+めるてぃ
+メルティ工房
+メルロー
+も〜も〜ぼっくす（モウモウボックス）
+もーにんぐ
+もーにんぐ らいと
+もこプロ
+モニスタラッシュ / a Matures（モニスタラッシュアマチュアズ）
+モニスタラッシュ/a Matures
+もみじ
+ももいろPocket
+ももかん
+モンスターブラックマーケット [チーム-アップルパイ]
+ゆ～かりそふと
+ユーフォリア
+ユニゾンシフト・クレア
+ユメスタ
+ゆめまくら
+ゆりんゆりん
+ライアーソフト
+らずべり～
+らぱぷる
+ラブチェリー
+リーベ
+リトルウィッチ
+リトルウィッチ・ベルベット
+リンガーベル
+ルクス
+ルクス・もーしょん
+ルナソフト
+ルナティック
+ルネ
+ルネ loves K
+ルネ Team Bitters
+レイヴ
+ローズクラウン
+ローズティアラ
+わるきゅ〜れ（ワルキューレ）
+わるきゅ～れ
+暗黒劇場
+縁 -yukari-
+俺的嗜好（オレテキシコウ）
+下世話な話（ゲセワナハナシ）
+可憐ソフト
+雅
+甘辛でいこう（アマカラデイコウ）
+鬼畜野郎
+戯画
+戯画/TEAM BALDRHEAD
+牛乳ソフト（ミルクソフト）
+牛乳戦車
+暁WORKS-黒-
+極フェロ
+極フェロ | GOKU-FERO | 作品一覧
+玉藻スタジオ（タマモスタジオ）
+九頭龍
+駒屋
+幻覚堂
+幻斎
+幻遊郷
+黒†救
+黒色槍騎兵（コクショクソウキヘイ）
+黒雛
+黒鳥
+黒姫
+左13度
+彩[Xai]
+祭企画
+皐月
+自宅すたじお
+蛇ノ道ハ蛇ソフト
+汁・ザル
+汁ダク系 虹組
+汁ダク系 乳組
+熟れ専
+熟女時代
+女騎士の城（オンナキシノシロ）
+人生通行止め（ジンセイツウコウドメ）
+聖ミカエル女子学園
+青梅製作所
+赤箱 (あかべぇそふとつぅ)
+雪月花ソフトウェア
+雪苺ショコラ
+創美研究所
+蒼見鳥（アオミドリ）
+大吉 – 浮士德の旧作收集馆
+大熊猫
+丹下拳闘倶楽部（タンゲケントウクラブ）
+天狐
+天使と悪魔
+天沼矛（非公开） – 浮士德の旧作收集馆
+天津堂
+吐息屋
+桃フェロ
+桃源郷
+桃色劇場
+桃雛
+同人ゲームサークル　ぺぺろんちーの
+濡汁
+脳内彼女
+覇王
+覇王Darkness
+柏党
+白濁系
+白濁系カウパー
+美少女ゲームOnlinePartners
+美遊
+筆柿そふと
+風露
+萌。
+萌♂（モエオス）
+萌雛
+本気汁
+魔法使いソフト
+娘娘（ニャンニャン）
+目隠し娘（メカクシムスメ）
+野良ウサギ
+裸足少女
+乱蝶姫（みだれあげは）
+林組
+α-MODEL（アルファモデル）
+ωstar
+エアリーソックス
+鹿野工房
+くちなわ
+PRINCIPIA`
+
+export {str}
